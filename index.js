d0a2c8867098eae302dcad5be193ed10f4f0aa62
zhentaoo <1904974327@qq.com>
2017-08-17 11:10:16 +0800

    puppeterr test

---
+++ index.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://www.zhentaoo.com', {waitUntil: 'networkidle'});
+    await page.pdf({path: 'zhentaoo.pdf', format: 'A4'});
+
+    browser.close();
+})();
